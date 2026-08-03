f97b7b4ed67a1f013ca018d575ce6728b84c5fca
kiran yadav <kiran42727840@gmail.com>
2026-08-03 10:45:55 +0530

    Added PDF text chunking

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
-print("welcome to AI Career -Copilot")
+import streamlit as st
+from pypdf import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+st.title("📚 AI Study Assistant")
+st.write("upload PDFs and chat with them using AI!")
+name = st.text_input("Enter your name : ")
+if name:
+    st.write(f"Welcome {name}! 🚀")
+uploaded_file = st.file_uploader(
+    "upload your pdf",
+    type=["pdf"]
+)
+if uploaded_file:
+    st.write("PDF uploaded successfully!")
+    st.write(uploaded_file.name)
+
+    reader =PdfReader(uploaded_file)
+    #st.write(len(reader.pages))
+    #st.write(reader.pages[0])
+    #st.write(reader.pages[0].extract_text())
+    text =""
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+    
+    splitter =RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)#only created splitter not splitted yet
+    chunks =splitter.split_text(text)#actually splitting
+    st.write("number of chunks : ",len(chunks))
+    st.write("first chunk")
+    st.write(chunks[0])
+
+    st.write("PDF text extracted successfully!")
+    st.write(text[:1000])
+
